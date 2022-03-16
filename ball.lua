69c79a5cbbaa17f7274cd72f3d809627821f78dc
Immow <k.schippers@gmail.com>
2022-03-16 12:15:21 +0100

    adjusted some comments

--- ball.lua
+++ ball.lua
@@ -4,7 +4,7 @@ local enemy = require("enemy")
 local Ball = {}
 Ball.width = 10
 Ball.height = 10
-Ball.x = WINDOW_WIDTH / 2 - Ball.width / 2 -- center the ball on screen at start
+Ball.x = WINDOW_WIDTH / 2 - Ball.width / 2 -- place player paddle on the right side
 Ball.y = WINDOW_HEIGHT / 2 - Ball.height / 2 -- center the ball on screen at start
 Ball.xVelocity = 100
 Ball.yVelocity = 100
@@ -15,7 +15,7 @@ function Ball:stayInWindow()
 	end
 
 	if self.y + self.height > WINDOW_HEIGHT then
-		self.yVelocity = self.yVelocity * -1 -- when we reach the top we flip our speed to go the other way
+		self.yVelocity = self.yVelocity * -1 -- when we reach the bottom we flip our speed to go the other way
 	end
 end
 
